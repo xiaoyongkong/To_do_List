df39a21ce2bf200ac6121b1aab0e7e5fca0b4ac4
xiaofinancier <xiaoykong06@gmail.com>
2020-09-02 23:14:29 -0300

    full todo with login

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Todo(models.Model):
     text = models.CharField(max_length=40)
     complete = models.BooleanField(default=False)
-
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     def __str__(self):
       return self.text
--- todo/views.py
+++ todo/views.py
@@ -7,21 +7,19 @@ from .forms import TodoForm
 
 @login_required 
 def index(request):
-    todo_list = Todo.objects.order_by('id')
-
+    todo_list = Todo.objects.order_by('id').filter(user=request.user)
     form = TodoForm()
-
     context = {'todo_list' : todo_list, 'form' : form}
-
+    
     return render(request, 'index.html', context)
 
 @login_required
 @require_POST
 def addTodo(request):
     form = TodoForm(request.POST)
-
     if form.is_valid():
         new_todo = Todo(text=request.POST['text'])
+        new_todo.user = request.user
         new_todo.save()
 
     return redirect('index')
@@ -36,12 +34,12 @@ def completeTodo(request, todo_id):
 
 @login_required
 def deleteCompleted(request):
-    Todo.objects.filter(complete__exact=True).delete()
+    Todo.objects.filter(complete__exact=True, user=request.user).delete()
 
     return redirect('index')
 
 @login_required
 def deleteAll(request):
-    Todo.objects.all().delete()
+    Todo.objects.filter(user=request.user).delete()
 
     return redirect('index')
